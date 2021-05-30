acc94aa73fef4d1f91066674912d8934f7daa6af
Minor G <minorg@server.fake>
2021-05-29 20:09:41 -0400

    action working from command line

--- action.py
+++ action.py
@@ -8,8 +8,15 @@ from pathlib import Path
 import sys
 from typing import Optional
 
+from paradicms_etl.extractors.markdown_directory_extractor import (
+    MarkdownDirectoryExtractor,
+)
 from paradicms_etl._loader import _Loader
 from paradicms_etl.loaders.rdf_file_loader import RdfFileLoader
+from paradicms_etl._pipeline import _Pipeline
+from paradicms_etl.transformers.markdown_directory_transformer import (
+    MarkdownDirectoryTransformer,
+)
 
 
 class Action:
@@ -33,6 +40,12 @@ class Action:
                         kwds[field.name] = environ_value
             return cls(**kwds)
 
+    class __Pipeline(_Pipeline):
+        ID = "action"
+
+        def __init__(self, **kwds):
+            _Pipeline.__init__(self, id=self.ID, **kwds)
+
     def __init__(self, inputs: Optional[Inputs] = None):
         if inputs is None:
             inputs = Action.Inputs.from_environment()
@@ -47,14 +60,43 @@ class Action:
     def __create_loader(self) -> _Loader:
         if self.__inputs.output_format.endswith("-rdf"):
             rdf_file_path = Path(self.__inputs.output_data)
-            rdf_format = self.inputs.output_format[: -len("-rdf")]
+            rdf_format = self.__inputs.output_format[: -len("-rdf")]
             self.__mkdir(rdf_file_path.parent)
-            self.__logger(
+            self.__logger.debug(
                 "RDF file loader: format=%s, file path=%s", rdf_format, rdf_file_path
             )
-            return RdfFileLoader(file_path=rdf_file_path, format=rdf_format)
+            return RdfFileLoader(
+                file_path=rdf_file_path,
+                format=rdf_format,
+                pipeline_id=self.__Pipeline.ID,
+            )
         else:
             raise NotImplementedError(self.__inputs.output_format)
+
+    def __create_markdown_directory_pipeline(self, *, loader: _Loader) -> _Pipeline:
+        extracted_data_dir_path = Path(self.__inputs.input_data)
+        if not extracted_data_dir_path.is_dir():
+            raise ValueError(
+                f"Markdown directory {extracted_data_dir_path} does not exist"
+            )
+
+        return self.__Pipeline(
+            extractor=MarkdownDirectoryExtractor(
+                extracted_data_dir_path=extracted_data_dir_path,
+                pipeline_id=self.__Pipeline.ID,
+            ),
+            loader=loader,
+            transformer=MarkdownDirectoryTransformer(
+                pipeline_id=self.__Pipeline.ID,
+            ),
+        )
+
+    def __create_pipeline(self) -> _Pipeline:
+        loader = self.__create_loader()
+        if self.__inputs.input_format in ("markdown", "markdown_directory"):
+            return self.__create_markdown_directory_pipeline(loader=loader)
+        else:
+            raise NotImplementedError(self.__inputs.input_format)
 
     def __mkdir(self, dir_path: Path) -> None:
         if dir_path.is_dir():
@@ -67,7 +109,8 @@ class Action:
             self.__logger.debug("created directory %s", dir_path)
 
     def run(self):
-        pass
+        pipeline = self.__create_pipeline()
+        pipeline.extract_transform_load()
 
 
 if __name__ == "__main__":
