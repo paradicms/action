bda678f61876e290ae1885ecc986f9783ff9b1d9
Minor Gordon <minorg@server.fake>
2023-03-22 11:26:20 -0400

    update action.py and action.yml to fit with paradicms

--- action.py
+++ action.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
+import dataclasses
 from dataclasses import dataclass
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from typing import Optional
 from urllib.parse import urlparse
 
@@ -12,37 +11,35 @@ from paradicms_etl.extractors.google_sheets_extractor import GoogleSheetsExtract
 from paradicms_etl.pipeline import Pipeline
 from paradicms_etl.transformers.spreadsheet_transformer import SpreadsheetTransformer
 from paradicms_ssg.git_hub_action import GitHubAction
+from paradicms_ssg.github_actions_inputs import GitHubActionInputs
 from paradicms_ssg.models.root_model_classes_by_name import ROOT_MODEL_CLASSES_BY_NAME
 
 
-class Action(GitHubAction):
-    @dataclass(frozen=True)
-    class RequiredInputs(GitHubAction.RequiredInputs):
-        spreadsheet: str
+@dataclass(frozen=True)
+class _Inputs(GitHubActionInputs):
+    spreadsheet: str = dataclasses.field(
+        default=GitHubActionInputs.REQUIRED,
+        metadata={
+            "description": "Google Sheets spreadsheet id, Google Sheet URL, or path to an Excel 2010 (.xlsx) file"
+        },
+    )
 
-    @dataclass(frozen=True)
-    class Inputs(GitHubAction.OptionalInputs, RequiredInputs):
-        pass
 
-    def __init__(self, *, inputs: Inputs, temp_dir_path: Path):
-        GitHubAction.__init__(
-            self,
-            optional_inputs=inputs,
-            required_inputs=inputs,
-            temp_dir_path=temp_dir_path,
-        )
-        self.__inputs = inputs
+class Action(GitHubAction[_Inputs]):
+    """
+    Generate a static site from a Paradicms-formatted spreadsheet.
+    """
 
     @classmethod
-    def main(cls):
-        with TemporaryDirectory() as temp_dir:
-            cls(inputs=cls.Inputs.from_args(), temp_dir_path=Path(temp_dir)).__main()
+    @property
+    def _inputs_class(cls):
+        return _Inputs
 
-    def __main(self):
+    def _run(self):
         # https://docs.google.com/spreadsheets/d/1j2oaMvMxY4pnXO-sEH_fky2R2gm6TQeIev_Q8rVOD4M/edit?usp=sharing
         extractor: Optional[Extractor] = None
         try:
-            spreadsheet_url = urlparse(self.__inputs.spreadsheet)
+            spreadsheet_url = urlparse(self._inputs.spreadsheet)
             if spreadsheet_url.hostname == "docs.google.com":
                 if spreadsheet_url.path.startswith("/spreadsheets/d/"):
                     extractor = GoogleSheetsExtractor(
@@ -54,7 +51,7 @@ class Action(GitHubAction):
         except ValueError:
             pass
 
-        spreadsheet_file_path = Path(self.__inputs.spreadsheet)
+        spreadsheet_file_path = Path(self._inputs.spreadsheet)
         if spreadsheet_file_path.is_file():
             if str(spreadsheet_file_path).lower().endswith(".xlsx"):
                 extractor = Excel2010Extractor(xlsx_file_path=spreadsheet_file_path)
@@ -62,15 +59,15 @@ class Action(GitHubAction):
         if extractor is None:
             extractor = GoogleSheetsExtractor(
                 extracted_data_dir_path=self._extracted_data_dir_path,
-                spreadsheet_id=self.__inputs.spreadsheet,
+                spreadsheet_id=self._inputs.spreadsheet,
             )
 
         Pipeline(
             extractor=extractor,
-            id=self.__inputs.pipeline_id,
+            id=self._inputs.pipeline_id,
             loader=self._create_loader(),
             transformer=SpreadsheetTransformer(
-                pipeline_id=self.__inputs.pipeline_id,
+                pipeline_id=self._inputs.pipeline_id,
                 root_model_classes_by_name=ROOT_MODEL_CLASSES_BY_NAME,
             ),
         ).extract_transform_load()
